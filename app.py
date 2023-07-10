a6f24fa91a249f4fca78b8bf3ee22bc3e2a03e5d
RachelBra <racheliur10@gmail.com>
2023-07-10 15:54:58 +0300

    read+save cifar-10

--- app.py
+++ app.py
@@ -7,28 +7,37 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-path = r'C:\bootCamp\APPLIED\cifar-10-batches-py\\data_batch_1'
-data_dict = unpickle(path)
+path = r'C:\bootCamp\APPLIED\cifar-10-batches-py\\data_batch_'
+for i in range(1, 6):
+    data_dict = unpickle(path+str(i))
+    data = data_dict[b'data']
+    labels = data_dict[b'labels']
 
 
-data = data_dict[b'data']
-labels = data_dict[b'labels']
+    images = np.reshape(data, (len(data), 3, 32, 32))
+    print("images", images)
 
+    fig, axes = plt.subplots(2, 5, figsize=(12, 6))
+    axes = axes.ravel()
+    # Saving the images
+    output_dir = r'C:\bootCamp\APPLIED\output_images'
+    image_format = 'png'
 
-images = np.reshape(data, (len(data), 3, 32, 32))
-print("images", images)
+    for j in range(len(images)):
+        image = images[j]
+        image = np.transpose(image, (1, 2, 0))
 
-fig, axes = plt.subplots(2, 5, figsize=(12, 6))
-axes = axes.ravel()
+        label = labels[j]
+        image_name = 'image_{}_label_{}.{}'.format(j + 1, label, image_format)
+        output_path = output_dir + '\\' + image_name
 
-for i in range(10):
-    image = images[i]
-    image = np.transpose(image, (1, 2, 0))
+        plt.imshow(image)
+        plt.axis('off')
+        plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
+        plt.close()
 
-    axes[i].imshow(image)
-    axes[i].set_title('Label: {}'.format(labels[i]))
-    axes[i].axis('off')
+    print('Images saved successfully!')
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
 
