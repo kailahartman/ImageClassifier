82e50ef21d2ef7c037aa9cb0792049d3a69cff83
Tamargoytin <137270701+Tamargoytin@users.noreply.github.com>
2023-07-12 21:44:57 +0300

    Update divideToTrainAndTest.py

--- preprocess/divideToTrainAndTest.py
+++ preprocess/divideToTrainAndTest.py
@@ -5,8 +5,12 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv("data.csv")
 
 # Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(df, df["target"], test_size=0.2)
+# x=df.loc[:,['Industry_code_ANZSIC06','Industry_aggregation_NZSIOC','Year']]
+# y=df.loc[:,['Value']]
+X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
 
 # Save the train and test sets
 X_train.to_csv("CFARTrainData.csv")
 X_test.to_csv("CFARTestData.csv")
+y_test.to_csv("CFARTestDataY.csv")
+y_train.to_csv("CFARTrainDataY.csv")
