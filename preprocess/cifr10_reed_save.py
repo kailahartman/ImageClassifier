c54834bc944ef5b22555e984f832d6e8bfd14647
RachelBra <137271493+RachelBra@users.noreply.github.com>
2023-07-12 14:18:16 +0300

    Update cifr10_reed_save.py

--- preprocess/cifr10_reed_save.py
+++ preprocess/cifr10_reed_save.py
@@ -23,13 +23,13 @@ def save_image_local(images, output_dir, image_format):
         plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
         plt.close()
 
-path = r'C:\bootCamp\APPLIED\cifar-10-batches-py\\data_batch_'
-for i in range(3, 6):
+path = r'C:\bootCamp\APPLIED\cifar-10-batches-py\\data_batch_'  #your path
+for i in range(1, 6):
     data_dict = reed_data(path + str(i))
     data = data_dict[b'data']
     labels = data_dict[b'labels']
     images = np.reshape(data, (len(data), 3, 32, 32))
-    output_dir = r'C:\bootCamp\APPLIED\output_images_' + str(i)
+    output_dir = r'C:\bootCamp\APPLIED\output_images_' + str(i) #your path
     image_format = 'png'
     save_image_local(images, output_dir, image_format)
 
