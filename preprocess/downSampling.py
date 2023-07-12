bec4b38464996c62b69824c645a2c0a144f934f8
kailahartman <kailahartman123@gmail.com>
2023-07-12 14:17:01 +0300

    better downSampling

--- preprocess/downSampling.py
+++ preprocess/downSampling.py
@@ -1,28 +1,10 @@
-import numpy as np
-import cv2
+from PIL import Image
 
-def downsample_image(image, new_size):
-  """Downsamples an image to the specified size."""
-  old_size = image.shape[:2]
-  new_image = np.zeros((new_size[0], new_size[1], 3))
-  for i in range(new_size[0]):
-    for j in range(new_size[1]):
-      old_i = int(i * old_size[0] / new_size[0])
-      old_j = int(j * old_size[1] / new_size[1])
-      new_image[i, j] = image[old_i, old_j]
-  return new_image
+# Load the original image
+image = Image.open("hi.png")
 
-def read_image(image_path):
-  """Reads an image from the specified path."""
-  image = cv2.imread(image_path)
-  return image
+# Resize the image to 32x32 pixels
+resized_image = image.resize((32, 32), Image.ANTIALIAS)
 
-def write_image(image, image_path):
-  """Writes an image to the specified path."""
-  cv2.imwrite(image_path, image)
-
-image_path = "car.png"
-image = read_image(image_path)
-new_image = downsample_image(image, (32, 32))
-write_image(new_image, "new_image.png")
-
+# Save the resized image
+resized_image.save("resized_image.png")
