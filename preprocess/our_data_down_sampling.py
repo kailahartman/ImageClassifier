54cef350b9e4a468d5ac65d79e3336c3eb2ab6aa
tamarGoytin <tamar6021@gmail.com>
2023-07-17 12:58:20 +0300

    UPDATE PATH

--- preprocess/our_data_down_sampling.py
+++ preprocess/our_data_down_sampling.py
@@ -6,15 +6,14 @@ import os
 def down_sampling(load_path, save_path):
     image = Image.open(load_path)
     resized_image = image.resize((32, 32), Image.LANCZOS)
-
     umpydata = np.asarray(resized_image)
     np.save(save_path, umpydata)
 
 
 
 
-folder_load_path = r'C:\bootcamp\project\our_data_images'  # change to your path
-folder_save_path = r"C:\bootCamp\project\our_data"
+folder_load_path = os.getcwd()+"data/our_data_umge from _google" # change to your path
+folder_save_path = os.getcwd()+"data/our_data_downsampled"
 for file_name in os.listdir(folder_load_path):
     load_path = os.path.join(folder_load_path, file_name)
     save_path = os.path.join(folder_save_path, "our_data")
