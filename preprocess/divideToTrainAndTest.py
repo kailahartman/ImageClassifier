266729eb899ecff642d75cc7442056ebea1718ba
kailahartman <kailahartman123@gmail.com>
2023-07-17 09:32:26 +0300

    update train and test

--- preprocess/divideToTrainAndTest.py
+++ preprocess/divideToTrainAndTest.py
@@ -5,8 +5,8 @@ path_to_data = os.getcwd()+r'\\data\\combined_data.csv'
 
 df = pd.read_csv(path_to_data)
 
-X = df.loc[:, ['label', 'source']]
-y = df.loc[:, ['path']]
+X = df.loc[:, ['path', 'source']]
+y = df.loc[:, ['label']]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Save the train and test sets
