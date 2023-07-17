66a0639af9e175b120c865826a6bac77a481aa1b
RachelBra <racheliur10@gmail.com>
2023-07-17 16:14:55 +0300

    cifr100

--- preprocess/our_data_down_sampling.py
+++ preprocess/our_data_down_sampling.py
@@ -9,11 +9,8 @@ def down_sampling(load_path, save_path):
     umpydata = np.asarray(resized_image)
     np.save(save_path, umpydata)
 
-
-
-
-folder_load_path = os.getcwd()+"data/our_data_umge from _google" # change to your path
-folder_save_path = os.getcwd()+"data/our_data_downsampled"
+folder_load_path = os.path.join(os.getcwd(), r'data\our data\our_data_umge from _google')
+folder_save_path = os.path.join(os.getcwd(), r'data\our data')
 for file_name in os.listdir(folder_load_path):
     load_path = os.path.join(folder_load_path, file_name)
     save_path = os.path.join(folder_save_path, "our_data")
