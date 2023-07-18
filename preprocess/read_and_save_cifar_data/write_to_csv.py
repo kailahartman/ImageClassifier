1df5b2b5f25167e66b56ec95c213b2c18cf2e31b
kailahartman <kailahartman123@gmail.com>
2023-07-18 20:55:57 +0300

    outside create csv function - labels match

--- preprocess/read_and_save_cifar_data/write_to_csv.py
+++ preprocess/read_and_save_cifar_data/write_to_csv.py
@@ -10,13 +10,20 @@ def extract_label(string):
     else:
         return None
 
-def save_cifar_to_csv(labels, image_dir, output_file, dataset_name):
+def save_cifar_to_csv(image_dir, output_file, dataset_name):
     paths = []
+    labels = []
 
     for file_name in os.listdir(image_dir):
         file_path = os.path.join(image_dir, file_name)
         if os.path.isfile(file_path):
             paths.append(file_path)
+            label = extract_label(file_path)
+            if label is not None:
+                labels.append(label)
+            else:
+                print("err", file_path)
+
 
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
