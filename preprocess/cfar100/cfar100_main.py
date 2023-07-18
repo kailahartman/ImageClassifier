0fb99bd1ddb376e2c7e1e7a0d99b8fedaff57a97
kailahartman <kailahartman123@gmail.com>
2023-07-18 16:57:51 +0300

    change cfar100

--- preprocess/cfar100/cfar100_main.py
+++ preprocess/cfar100/cfar100_main.py
@@ -3,7 +3,7 @@ import os
 from preprocess.read_and_save_data.read_data import read_data
 from preprocess.read_and_save_data.save_as_images_locally import save_image_local
 from preprocess.read_and_save_data.save_to_numpy_file import save_as_numpy_file
-#from cfar100_create_csv import save_cifar100_to_csv
+# from cfar100_create_csv import save_cifar100_to_csv
 
 def cfar100_read_save_locally_numpy_csv():
     file = os.getcwd()+r'\\data\\cifar-100-python\train'
@@ -18,7 +18,7 @@ def cfar100_read_save_locally_numpy_csv():
             list_lable.append(train_data[b'coarse_labels'][i])
     save_as_numpy_file(output_file,list_lable,list_data)
     # flowers=2  fish=1 people=14
-    # new_dict={2:[],1:[],17:[]}
+    #new_dict={2:[],1:[],17:[]}
     # for i in range(0,len(train_data[b'coarse_labels'])):
     #     num=train_data[b'coarse_labels'][i]
     #     if(num==2 or num==1 or num==17):
@@ -26,10 +26,10 @@ def cfar100_read_save_locally_numpy_csv():
     # for i in new_dict.keys():
     #     data = new_dict[i]
     #     labels = [i]*len(new_dict[i])
-    #     images = np.reshape(data, (len(data), 3, 32, 32))
-    #     output_dir = os.getcwd()+r'\\data\\cifar100_class_' + str(i)
-    #     image_format = 'png'
-    #     save_image_local(images, output_dir, image_format,labels)
+    images = np.reshape(list_data, (len(list_data), 3, 32, 32))
+    output_dir = os.getcwd()+r'\\data\\output_images_from_cifar100'
+    image_format = 'png'
+    save_image_local(list_data, output_dir, image_format,list_lable)
     #
     #
     # output_dir = os.getcwd()+r'\\data'
