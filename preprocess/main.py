f2c0c410c5fbfaa56bdaffa1ea2eaebff2051e98
kailahartman <kailahartman123@gmail.com>
2023-07-18 21:03:44 +0300

    outside create csv function - labels match

--- preprocess/main.py
+++ preprocess/main.py
@@ -1,6 +1,6 @@
 
-from cfar10.cfar10_main import cfar10_read_save_locally_numpy_csv
-from cfar100.cfar100_main import cfar100_read_save_locally_numpy_csv
+from read_and_save_cifar_data.cfar10_main import cfar10_read_save_locally_numpy_csv
+from read_and_save_cifar_data.cfar100_main import cfar100_read_save_locally_numpy_csv
 
 from preprocess.our_data.our_data_creat_csv import create
 from preprocess.our_data.our_data_down_sampling import downsampling
@@ -9,7 +9,7 @@ from preprocess.our_data.our_data_save_as_numpy import save_as_numpy
 # from preprocess.additional_operations.normalization import normalization
 
 
-# cfar10_read_save_locally_numpy_csv()
+cfar10_read_save_locally_numpy_csv()
 cfar100_read_save_locally_numpy_csv()
 #
 # create()
