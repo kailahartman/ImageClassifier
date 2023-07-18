d433525de3ba4a2f2919d642e3a0dc78ebd30c57
RachelBra <racheliur10@gmail.com>
2023-07-18 11:22:17 +0300

    temperory

--- preprocess/show_data.py
+++ preprocess/show_data.py
@@ -1,4 +1,16 @@
 import numpy as np
 import os
 
-data = np.load(os.path.join(os.getcwd(), r'data\our data'), allow_pickle=True)
+#
+
+def read_cifar10_from_numpy_dict(file_path):
+    cifar_data = np.load(file_path, allow_pickle=True)
+    images = cifar_data['images']
+    labels = cifar_data['labels']
+    return images, labels
+
+file_path = os.path.join(os.getcwd(), 'data\our data', 'our_data.npz')
+images, labels = read_cifar10_from_numpy_dict(file_path)
+print("images:", images)
+print("labels:", labels)
+
