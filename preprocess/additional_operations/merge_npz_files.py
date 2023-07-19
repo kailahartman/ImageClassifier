c09d55e7c15eb387e6610d9416bd332f8071894b
tamarGoytin <tamar6021@gmail.com>
2023-07-19 14:57:25 +0300

    update merge function

--- preprocess/additional_operations/merge_npz_files.py
+++ preprocess/additional_operations/merge_npz_files.py
@@ -3,5 +3,8 @@ import numpy as np
 def merge():
     x = np.load(os.getcwd()+r'\\data\\cfar10.npz')
     y = np.load(os.getcwd()+r'\\data\\cfar100.npz')
-    z=[*x, *y]
-    np.savez('data\merged_data.npz',z)
+    z = np.load(os.getcwd()+r'\\data\\custom_data.npz')  #rename the name of your file
+
+    merge=[*x, *y, *z]
+    np.savez('data\merged_data.npz',merge)
+
