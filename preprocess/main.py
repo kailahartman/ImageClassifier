55037e35fd2e6b7ff9abe2ab112ab248b80ffc6a
kailahartman <kailahartman123@gmail.com>
2023-07-23 18:35:19 +0300

    custom data matches labels

--- preprocess/main.py
+++ preprocess/main.py
@@ -7,16 +7,16 @@ from additional_operations.combine_csv_files import combine_csv_files
 from additional_operations.normalization import scale_images_numpy_array
 from additional_operations.our_data_train_test import split_train_test
 from visualize.show_data import show_data_main
+import os
 
-
-cfar10_read_save_locally_numpy_csv()
-cfar100_read_save_locally_numpy_csv()
+# cfar10_read_save_locally_numpy_csv()
+# cfar100_read_save_locally_numpy_csv()
 custom_images_read_save_locally_numpy_csv()
-combine_csv_files()
-#
-merge_npz_files()
-split_train_test()
-# scale_images_numpy_array()
-split_train_test()
+# combine_csv_files()
+# #
+# merge_npz_files()
+# split_train_test()
+# # scale_images_numpy_array()
+# split_train_test()
 
 show_data_main()
