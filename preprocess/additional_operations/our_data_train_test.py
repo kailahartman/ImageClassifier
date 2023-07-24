4bc3e149a216f38fa77fec07354876973dcb0a3a
RachelBra <137271493+RachelBra@users.noreply.github.com>
2023-07-24 12:57:08 +0300

    Update our_data_train_test.py

--- preprocess/additional_operations/our_data_train_test.py
+++ preprocess/additional_operations/our_data_train_test.py
@@ -13,21 +13,14 @@ def division(images,labels):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
-
-def save_as_numpy_file(output_file, data,name):
-    # train_labels = np.array(labels)
-    cifar_dict = {name: data}
-    np.savez(output_file, **cifar_dict)
-
-
 def save_numpy_file(parent_dir,images,labels):
     print("saving the train and test to a numpy file")
     X_train, X_test, y_train, y_test = division(images, labels)
     l = [X_train, X_test, y_train, y_test]
     names = ['X_train', 'X_test', 'y_train', 'y_test']
-    for i in range(len(l)):
-        file_path1 = os.path.join(os.getcwd(), 'data', names[i])
-        save_as_numpy_file(file_path1, l[i], names[i])
+    for i, j in zip(l, names):
+        file_path1 = os.path.join(os.getcwd(), 'data', j)
+        np.save(file_path1, i)
 
 def split_train_test():
 
@@ -35,6 +28,7 @@ def split_train_test():
     file_path = os.path.join(os.getcwd(), 'data', 'merged_data.npz')
     images, labels = read_from_numpy_dict(file_path)
     save_numpy_file(parent_dir,images,labels)
+
 
 
 
