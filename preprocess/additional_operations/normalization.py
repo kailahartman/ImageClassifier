9930228b00d60599ffd029ba4096851ab50556c4
kailahartman <kailahartman123@gmail.com>
2023-07-24 11:50:20 +0300

    working on normilization

--- preprocess/additional_operations/normalization.py
+++ preprocess/additional_operations/normalization.py
@@ -1,16 +1,17 @@
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 from preprocess.read_and_save_cifar_data.save_to_numpy_file import save_as_numpy_file
-def standardize_data(data):
-    # Reshape the data array from 4 dimensions to 2 dimensions
-    flattened_data = data.reshape(data.shape[0], -1)
-    scaler = StandardScaler()
-    scaled_data = scaler.fit_transform(flattened_data)
-    return scaled_data
 
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# Convert class vectors to binary class matrices. This is called one hot encoding.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
 def scale_images_numpy_array():
     print("normalizing the merged npz file")
     data = np.load("data/merged_data.npz", allow_pickle=True)
     scaled_data = standardize_data(data['images'])
-    save_as_numpy_file(r"data/normalization",data['labels'],scaled_data)
+    save_as_numpy_file(r"data/normalization", data['labels'], scaled_data)
